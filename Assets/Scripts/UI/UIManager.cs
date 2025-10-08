2b81b0a219784d4f8232286a320290fafa5456dd
defredus <defredzte@gmail.com>
2025-10-08 19:14:08 +0300

    pause menu started

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -3,13 +3,27 @@ using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
+	[Header("Game Over")]
 	[SerializeField] private GameObject _gameOverScreen;
 	[SerializeField] private AudioClip _gameOverSound;
+
+	[Header("Pause")]
+	[SerializeField] private GameObject _pauseScreen;
+
 
 	private void Awake()
 	{
 		_gameOverScreen.SetActive(false);
+		_pauseScreen.SetActive(false);
 	}
+	private void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Escape))
+			PauseGame(true);
+		else
+			PauseGame(false);
+	}
+	#region Game Over
 	public void GameOver()
 	{
 		_gameOverScreen.SetActive(true);
@@ -26,8 +40,21 @@ public class UIManager : MonoBehaviour
 	public void Quit()
 	{
 		Application.Quit(); //Quits the game(only in build version)
-		UnityEditor.EditorApplication.isPlaying = false; //Exits Play Mode
 
+		#if UNITY_EDITOR
+			UnityEditor.EditorApplication.isPlaying = false; //Exits Play Mode
+		#endif
 	}
+	#endregion
 
+	#region Pause
+	private void PauseGame(bool status)
+	{
+		_pauseScreen.SetActive(status);
+		if(status)
+			Time.timeScale = 0;
+		else
+			Time.timeScale = 1;
+	}
+	#endregion
 }
